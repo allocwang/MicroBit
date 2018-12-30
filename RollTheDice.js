8bc16f59894f6caa50654e507e9f1fa7109409e5
Caleb <allocwang@gmail.com>
2018-12-30 20:18:52 +0800

    RollTheDice

---
+++ RollTheDice.js
@@ -0,0 +1,108 @@
+let roll = 0
+let times = 0
+let num = 0
+input.onGesture(Gesture.Shake, function () {
+    num = Math.randomRange(1, 6)
+    times = Math.randomRange(4, 8)
+    for (let index = 0; index <= times; index++) {
+        roll = Math.randomRange(1, 6)
+        if (roll == 1) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+        } else if (roll == 2) {
+            basic.showLeds(`
+                . . . . .
+                . # . . .
+                . . . . .
+                . . . # .
+                . . . . .
+                `)
+        } else if (roll == 3) {
+            basic.showLeds(`
+                . . . . .
+                . # . . .
+                . . # . .
+                . . . # .
+                . . . . .
+                `)
+        } else if (roll == 4) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                . # . # .
+                . . . . .
+                `)
+        } else if (roll == 5) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . # . .
+                . # . # .
+                . . . . .
+                `)
+        } else if (roll == 6) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . # . # .
+                . # . # .
+                . . . . .
+                `)
+        }
+    }
+    if (num == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (num == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (num == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (num == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (num == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (num == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
